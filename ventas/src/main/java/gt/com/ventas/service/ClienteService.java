c50f17357785b3a2fb1cfd76fdcba0d41168cf8d
Marvin Lopez <lopez.marvin9@gmail.com>
2022-03-04 13:31:41 -0600

    Agregando capa Service del cliente

---
+++ ventas/src/main/java/gt/com/ventas/service/ClienteService.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gt.com.ventas.service;
+
+import gt.com.ventas.model.Cliente;
+import gt.com.ventas.repository.IClienteRepository;
+import java.util.ArrayList;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author malopez
+ */
+@Service
+public class ClienteService implements IClienteService {
+
+    @Autowired
+    private IClienteRepository clienteRepo;
+
+    @Override
+    public ArrayList<Cliente> findAll() {
+        return (ArrayList<Cliente>) clienteRepo.findAll();
+    }
+
+    @Override
+    public Optional<Cliente> findById(Integer id) {
+        return clienteRepo.findById(id);
+    }
+
+    @Override
+    public Cliente create(Cliente cliente) {
+        return clienteRepo.save(cliente);
+    }
+
+    @Override
+    public Cliente update(Cliente cliente, Integer id) {
+        return clienteRepo.save(cliente);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        clienteRepo.deleteById(id);
+    }
+
+}
---
+++ ventas/src/main/java/gt/com/ventas/service/IClienteService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package gt.com.ventas.service;
+
+import gt.com.ventas.model.Cliente;
+import java.util.ArrayList;
+import java.util.Optional;
+
+/**
+ *
+ * @author malopez
+ */
+public interface IClienteService {
+
+    ArrayList<Cliente> findAll();
+
+    Optional<Cliente> findById(Integer id);
+
+    Cliente create(Cliente cliente);
+
+    Cliente update(Cliente cliente, Integer id);
+
+    void delete(Integer id);
+
+}
