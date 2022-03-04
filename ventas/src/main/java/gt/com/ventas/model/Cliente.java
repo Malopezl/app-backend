b05596d6293767ee97ab0eb8b93a57bde1a3030f
Marvin Lopez <lopez.marvin9@gmail.com>
2022-03-04 12:05:40 -0600

    Agregando modelo cliente

---
+++ ventas/src/main/java/gt/com/ventas/model/Cliente.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gt.com.ventas.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+/**
+ *
+ * @author malopez
+ */
+@Entity
+@Data
+@Table(name = "Cliente")
+public class Cliente implements Serializable {
+
+    @Id
+    @Column(name = "idCliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCliente;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+}
